285db5a8999dbfb6383e2b0fc3c6c39426cfe1ab
Paul Stadig <paul@stadig.name>
2014-11-27 14:27:45 -0500

    A new adventure begins...

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject alice "0.0.1-SNAPSHOT"
+  :description "A cryptographic library for Clojure."
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :global-vars {*warn-on-reflection* true})
